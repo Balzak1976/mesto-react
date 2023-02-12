b72e578067d29749b1080a9be4640b1c70ecc6d4
Max Skorokhodov <skormaksim@mail.ru>
2023-02-12 17:27:49 +0300

    hotfix: удалить App.test.js, перенести деструктуризацию в вызовы, установить минимальную высоту html страницы

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,39 +1,36 @@
 import React from 'react';
 
-function Card({ props, onCardClick }) {
-  const { name, link, likes } = props;
+function Card({ props: { name, link, likes }, onCardClick }) {
 
   return (
-    <>
-      <li className='card__item'>
-        <article className='card'>
-          <button
-            className='card__del-button card__del-button_hidden'
-            type='button'
-            aria-label='удалить'
-          ></button>
-          <img
-            className='card__image'
-            onClick={onCardClick}
-            src={link}
-            alt={name}
-          />
-          <div className='card__body'>
-            <h2 className='card__title'>{name}</h2>
-            <div className='card__like'>
-              <button
-                className='card__like-button'
-                type='button'
-                aria-label='лайкнуть'
-              ></button>
-              <div className='card__like-number'>
-                {likes.length ? likes.length : null}
-              </div>
+    <li className='card__item'>
+      <article className='card'>
+        <button
+          className='card__del-button card__del-button_hidden'
+          type='button'
+          aria-label='удалить'
+        ></button>
+        <img
+          className='card__image'
+          onClick={onCardClick}
+          src={link}
+          alt={name}
+        />
+        <div className='card__body'>
+          <h2 className='card__title'>{name}</h2>
+          <div className='card__like'>
+            <button
+              className='card__like-button'
+              type='button'
+              aria-label='лайкнуть'
+            ></button>
+            <div className='card__like-number'>
+              {likes.length > 0 && likes.length}
             </div>
           </div>
-        </article>
-      </li>
-    </>
+        </div>
+      </article>
+    </li>
   );
 }
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,45 +1,41 @@
 import React from 'react';
 import Children from './Children';
 
-function PopupWithForm({ popupConfig, isOpen, onClose }) {
-  const { classNameModifier, title, btnTitle,children } = popupConfig;
- 
+function PopupWithForm({ popupConfig: { classNameModifier, title, btnTitle, children }, isOpen, onClose }) {
   return (
-    <>
-      <section
-        className={
-          `popup popup_type_${classNameModifier} ${isOpen && 'popup_opened'}`
-        }
-      >
-        <div className='popup__container'>
+    <section
+      className={
+        `popup popup_type_${classNameModifier} ${isOpen && 'popup_opened'}`
+      }
+    >
+      <div className='popup__container'>
+        <button
+          className='popup__close'
+          onClick={onClose}
+          type='button'
+          aria-label='закрыть'
+        ></button>
+        <h2 className='popup__title'>{title}</h2>
+
+        <form
+          className={`form form_type_${classNameModifier}`}
+          name={classNameModifier}
+          noValidate
+        >
+
+          {children && <Children props={children} />}
+          
           <button
-            className='popup__close'
-            onClick={onClose}
-            type='button'
-            aria-label='закрыть'
-          ></button>
-          <h2 className='popup__title'>{title}</h2>
-
-          <form
-            className={`form form_type_${classNameModifier}`}
-            name={classNameModifier}
-            noValidate
+            className='form__submit'
+            name='submit'
+            type='submit'
+            disabled
           >
-
-            {children && <Children props={children} />}
-            
-            <button
-              className='form__submit'
-              name='submit'
-              type='submit'
-              disabled
-            >
-              {btnTitle}
-            </button>
-          </form>
-        </div>
-      </section>
-    </>
+            {btnTitle}
+          </button>
+        </form>
+      </div>
+    </section>
   );
 }
 
