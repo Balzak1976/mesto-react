c8b24fe2bf08ebd158cf11dd4a89cd0e2734ba77
Max Skorokhodov <skormaksim@mail.ru>
2023-02-25 22:44:56 +0300

    hotfix: убрать лишний React из import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import { api } from '../utils/Api';
 import { popupConfig } from '../utils/settings';
